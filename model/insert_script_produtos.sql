eef37a5ca188ddc9dcae4c6207ee17ac40870777
Daniel Moura <daniel31032003@gmail.com>
2024-04-02 22:00:14 -0300

    add sql scripts e nodemon

---
+++ model/insert_script_produtos.sql
@@ -0,0 +1,13 @@
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Playstation 5','Console da Sony','4000.00','02/04/2024');
+
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Xbox Series X','Console da Microsofit','3000.00','02/04/2024');
+
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Nitendo Switch','Console da Nitendo','2500.00','02/04/2024');
+
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Notebook Dell Inspirion','Notebook Gamer, da Dell','5000.00','02/04/2024');
+
+
